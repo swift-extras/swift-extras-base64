2a1172b26a63cc09bbd0b65ae44e89737ee80a68
Fabian Fett <fabianfett@mac.com>
2019-12-20 18:26:59 +0100

    Run performance against NIOWebsocket Base64

--- Sources/Base64PerformanceTest/main.swift
+++ Sources/Base64PerformanceTest/main.swift
@@ -3,7 +3,7 @@ import Base64
 
 let base64 = "AAECAwQFBgcICQoLDA0ODxAREhMUFRYXGBkaGxwdHh8gISIjJCUmJygpKissLS4vMDEyMzQ1Njc4OTo7PD0+P0BBQkNERUZHSElKS0xNTk9QUVJTVFVWV1hZWltcXV5fYGFiY2RlZmdoaWprbG1ub3BxcnN0dXZ3eHl6e3x9fn+AgYKDhIWGh4iJiouMjY6PkJGSk5SVlpeYmZqbnJ2en6ChoqOkpaanqKmqq6ytrq+wsbKztLW2t7i5uru8vb6/wMHCw8TFxsfIycrLzM3Oz9DR0tPU1dbX2Nna29zd3t/g4eLj5OXm5+jp6uvs7e7v8PHy8/T19vf4+fr7/P3+/w=="
 
-let runs = 1000000
+let runs = 100000
 let bytes = Array(UInt8(0)...UInt8(255))
 
 @discardableResult
@@ -21,13 +21,19 @@ print("------------------------------------------")
 print("Encoding")
 
 let data = Data(bytes)
-let foundationEncoding = timing(name: "Foundation") {
+let foundationEncoding = timing(name: "Foundation  ") {
   for _ in 1...runs {
     _ = data.base64EncodedString()
   }
 }
 
-let base64Encoding = timing(name: "Base64    ") {
+let nioWebsocketEncoding = timing(name: "NIOWebsocket") {
+  for _ in 1...runs {
+    _ = String(niobase64Encoding: bytes)
+  }
+}
+
+let base64Encoding = timing(name: "Base64      ") {
   for _ in 1...runs {
     _ = String(base64Encoding: bytes)
   }
@@ -36,13 +42,13 @@ let base64Encoding = timing(name: "Base64    ") {
 print("------------------------------------------")
 print("Decoding")
 
-let foundationDecoding = timing(name: "Foundation") {
+let foundationDecoding = timing(name: "Foundation  ") {
   for _ in 1...runs {
     _ = Data(base64Encoded: base64)
   }
 }
 
-let base64Decoding = timing(name: "Base64    ") {
+let base64Decoding = timing(name: "Base64      ") {
   for _ in 1...runs {
     _ = try! base64.base64decoded()
   }
