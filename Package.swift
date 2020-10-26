bf4f1d2f20b864a965747aa3bedb37964ac9a8e6
Fabian Fett <fabianfett@mac.com>
2020-10-26 14:01:35 +0100

    perf tests rename

--- Package.swift
+++ Package.swift
@@ -6,24 +6,12 @@ import PackageDescription
 let package = Package(
     name: "swift-base64-kit",
     products: [
-        .library(
-            name: "Base64Kit",
-            targets: ["Base64Kit"]
-        ),
+        .library(name: "Base64Kit", targets: ["Base64Kit"]),
     ],
     dependencies: [],
     targets: [
-        .target(
-            name: "Base64KitPerformanceTest",
-            dependencies: ["Base64Kit"]
-        ),
-        .target(
-            name: "Base64Kit",
-            dependencies: []
-        ),
-        .testTarget(
-            name: "Base64KitTests",
-            dependencies: ["Base64Kit"]
-        ),
+        .target(name: "Base64Kit", dependencies: []),
+        .target(name: "PerformanceTest", dependencies: ["Base64Kit"]),
+        .testTarget(name: "Base64KitTests", dependencies: ["Base64Kit"]),
     ]
 )
