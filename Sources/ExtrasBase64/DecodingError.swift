74668816dda520abcd8361e87e9fec60a60c91d2
Patrick Freed <patrick.freed@mongodb.com>
2021-01-12 16:47:15 -0500

    nest definition of DecodingError in Base64 enum

--- Sources/ExtrasBase64/DecodingError.swift
+++ Sources/ExtrasBase64/DecodingError.swift
@@ -1,7 +1,8 @@
-
-public enum DecodingError: Error, Equatable {
-    case invalidLength
-    case invalidCharacter(UInt8)
-    case unexpectedPaddingCharacter
-    case unexpectedEnd
+public extension Base64 {
+    enum DecodingError: Error, Equatable {
+        case invalidLength
+        case invalidCharacter(UInt8)
+        case unexpectedPaddingCharacter
+        case unexpectedEnd
+    }
 }
