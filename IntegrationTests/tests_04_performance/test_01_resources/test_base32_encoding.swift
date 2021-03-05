87da4201596adb14112c05066497373c6cc7c3a6
Adam Fowler <adamfowler71@gmail.com>
2021-03-05 11:55:17 +0000

    Base32 integration tests

---
+++ IntegrationTests/tests_04_performance/test_01_resources/test_base32_encoding.swift
@@ -0,0 +1,14 @@
+import ExtrasBase64
+
+func run(identifier: String) {
+    let bytes = Array(UInt8(0) ... UInt8(255))
+    var base32: String?
+
+    measure(identifier: identifier) {
+        for _ in 0 ..< 1000 {
+            base32 = Base32.encodeString(bytes: bytes)
+        }
+
+        return base32?.count ?? 0
+    }
+}
